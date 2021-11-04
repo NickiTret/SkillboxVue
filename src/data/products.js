9a3b54545227b998c7e9a1fecda2d4fd5c855e4b
Никита Третьяков <tretjakov.nickit@yandex.ru>
2021-11-04 18:23:27 +0300

    4 commit

--- src/data/products.js
+++ src/data/products.js
@@ -3,140 +3,160 @@ export default [{
         categoryId: 3,
         title: "Радионяня Motorola MBP16",
         price: 3690,
-        image: '/img/radio.jpg'
+        image: '/img/radio.jpg',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 2,
         categoryId: 1,
         title: "Ультразвуковая зубная щётка Playbrush Smart Sonic",
         price: 5660,
-        image: '/img/toothbrush.jpg'
+        image: '/img/toothbrush.jpg',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 3,
         categoryId: 2,
         title: "Смартфон Xiaomi Mi Mix 3 6/128GB",
         price: 21790,
-        image: '/img/phone.jpg'
+        image: '/img/phone.jpg',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 4,
         categoryId: 6,
         title: "Электроскейт Razor Cruiser",
         price: 24690,
-        image: '/img/board.jpg'
+        image: '/img/board.jpg',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 5,
         categoryId: 2,
         title: "Смартфон Xiaomi Mi A3 4/64GB Android One",
         price: 14960,
-        image: '/img/phone-2.jpg'
+        image: '/img/phone-2.jpg',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 6,
         categoryId: 2,
         title: "Смартфон Xiaomi Redmi 6/128GB",
         price: 8960,
-        image: '/img/phone-3.jpg'
+        image: '/img/phone-3.jpg',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 7,
         categoryId: 6,
         title: "Электрический дрифт-карт Razor Crazy Cart",
         price: 39900,
-        image: '/img/bicycle.jpg'
+        image: '/img/bicycle.jpg',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 8,
         categoryId: 6,
         title: "Гироскутер Razor Hovertrax 2.0",
         price: 34900,
-        image: '/img/wheels.jpg'
+        image: '/img/wheels.jpg',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 9,
         categoryId: 6,
         title: "Детский трюковой самокат Razor Grom",
         price: 4990,
-        image: '/img/scooter.jpg'
+        image: '/img/scooter.jpg',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 10,
         categoryId: 6,
         title: "Роллерсёрф Razor RipStik Air Pro",
         price: 6690,
-        image: '/img/ripstik.jpg'
+        image: '/img/ripstik.jpg',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 11,
         categoryId: 7,
         title: "Наушники AirPods с беспроводным зарядным футляром",
         price: 16560,
-        image: '/img/airpods.jpg'
+        image: '/img/airpods.jpg',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 12,
         categoryId: 7,
         title: "Наушники Sony",
         price: 30690,
-        image: '/img/headphones.jpg'
+        image: '/img/headphones.jpg',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 13,
         categoryId: 4,
         title: "Утюг Philips GC4909/60 Azur",
         price: 7983,
-        image: '/img/utug.jfif'
+        image: '/img/utug.jfif',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 14,
         categoryId: 4,
         title: "Стиральная машина Indesit IWUB 4085",
         price: 12990,
-        image: '/img/stiralka.jfif'
+        image: '/img/stiralka.jfif',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 15,
         categoryId: 5,
         title: "Игровая приставка Microsoft Xbox Series S 512 ГБ",
         price: 28990,
-        image: '/img/xbox.jfif'
+        image: '/img/xbox.jfif',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 16,
         categoryId: 5,
         title: "Телевизор NanoCell LG 55SM9800 55",
         price: 82780,
-        image: '/img/tele.jfif'
+        image: '/img/tele.jfif',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 17,
         categoryId: 5,
         title: "Умный браслет Xiaomi Mi Smart Band 6 CN",
         price: 3090,
-        image: '/img/clock.png'
+        image: '/img/clock.png',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 18,
         categoryId: 5,
         title: "Bluetooth-адаптер Baseus Wireless Receiver черный",
         price: 729,
-        image: '/img/zaradka.jfif'
+        image: '/img/zaradka.jfif',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 19,
         categoryId: 4,
         title: "Увлажнитель воздуха Xiaomi Smartmi Zhimi Air Humidifier 2 CJXJSQ02ZM",
         price: 6750,
-        image: '/img/uvlaznitel.jfif'
+        image: '/img/uvlaznitel.jfif',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
     {
         id: 20,
         categoryId: 5,
         title: "Игровая приставка Nintendo Switch rev.2 32 ГБ",
         price: 21990,
-        image: '/img/switch.png'
+        image: '/img/switch.png',
+        colorArray: ['#222', '#8BE000', '#73B6EA']
     },
 
 ]
